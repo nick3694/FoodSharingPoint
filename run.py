bdcf3182364469a6fa59b16ed97cc13f1c47a96e
nick3694 <nickola36@gmail.com>
2021-01-03 18:38:40 +0100

    Alpha

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from fds import app   # import app automaticamente dal modulo __init__
-
+from fsp import app   # import app automaticamente dal modulo __init__
+import os
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=False) #in order to reach the external links
+    app.run(host='localhost',port=5000, debug=False) #in order to reach the external links
+    #os.system('lt --subdomain foodsharingpoint --port 5000')
