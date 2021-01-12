80771e6e4ef77b5affbadba6c01d8d61fda6151a
nick3694 <nickola36@gmail.com>
2021-01-12 01:33:49 +0100

    Alpha

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from fsp import app   # import app automaticamente dal modulo __init__
+from fsp import app   # import app from __init__ module
 import os
 
+"File .py necessary in order to run the Flas web application on localhost, the number port 9000 has been chosen arbitrarily"
+
+
 if __name__ == '__main__':
-    app.run(host='localhost',port=5000, debug=False) #in order to reach the external links
-    #os.system('lt --subdomain foodsharingpoint --port 5000')
+    app.run(host='localhost',port=9000, debug=False)
+    #os.system('lt --subdomain foodsharingpoint --port 9000') #http tunneling terminal additional command
